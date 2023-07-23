b7f9d2f6d493af384a2b1ccd35eb842d24621861
Gokhan <engokhangok@gmail.com>
2023-07-23 15:19:27 +0300

    Home Page Partiallar Ayrıldı.

---
+++ RealEstate.UI/ViewComponents/Default/_SearchBarPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate.UI.ViewComponents.Default
+{
+    public class _SearchBarPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
