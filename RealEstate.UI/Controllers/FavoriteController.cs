1ab869dc8e25deceac7dbdab6d381a1c7b041222
ilayda <ilaydaozken@gmail.com>
2023-07-30 20:53:20 +0300

    conrtoller

---
+++ RealEstate.UI/Controllers/FavoriteController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstate.BLL.Abstract;
+using RealEstate.Entities.Entities;
+
+namespace RealEstate.UI.Controllers
+{
+    public class FavoriteController : Controller
+    {
+        private readonly IFavoriteService _favoriteService;
+
+        public FavoriteController(IFavoriteService favoriteService)
+        {
+            _favoriteService = favoriteService;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            List<Favorite> favorites = await _favoriteService.TGetAllAsync();
+            return View(favorites);
+        }
+
+
+        public async Task<IActionResult> Details(Guid id)
+        {
+            Favorite favorite = await _favoriteService.TGetByIdAsync(id);
+            if (favorite == null)
+            {
+                return NotFound();
+            }
+
+            return View(favorite);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+    
+        [HttpPost]
+        public async Task<IActionResult> Create(Favorite favorite)
+        {
+            if (ModelState.IsValid)
+            {
+                await _favoriteService.TInsertAsync(favorite);
+                return RedirectToAction("Index");
+            }
+
+            return View(favorite);
+        }
+
+
+        public async Task<IActionResult> Update(Guid id)
+        {
+            Favorite favorite = await _favoriteService.TGetByIdAsync(id);
+            if (favorite == null)
+            {
+                return NotFound();
+            }
+            return View(favorite);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(Guid id, Favorite favorite)
+        {
+            if (id != favorite.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _favoriteService.TUpdateAsync(favorite);
+                return RedirectToAction("Index");
+            }
+
+            return View(favorite);
+        }
+
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            Favorite favorite = await _favoriteService.TGetByIdAsync(id);
+            if (favorite == null)
+            {
+                return NotFound();
+            }
+
+            return View(favorite);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            Favorite favorite = await _favoriteService.TGetByIdAsync(id);
+            if (favorite == null)
+            {
+                return NotFound();
+            }
+
+            _favoriteService.TDelete(favorite);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ RealEstate.UI/Controllers/PropertyController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstate.BLL.Abstract;
+using RealEstate.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RealEstate.UI.Controllers
+{
+    public class PropertyController : Controller
+    {
+        private readonly IPropertyService _propertyService;
+
+        public PropertyController(IPropertyService propertyService)
+        {
+            _propertyService = propertyService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Property> properties = await _propertyService.TGetAllAsync();
+            return View(properties);
+        }
+
+        public async Task<IActionResult> Details(Guid id)
+        {
+            Property property = await _propertyService.TGetByIdAsync(id);
+            if (property == null)
+            {
+                return NotFound();
+            }
+
+            return View(property);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Property property)
+        {
+            if (ModelState.IsValid)
+            {
+                await _propertyService.TInsertAsync(property);
+                return RedirectToAction("Index");
+            }
+
+            return View(property);
+        }
+
+        public async Task<IActionResult> Update(Guid id)
+        {
+            Property property = await _propertyService.TGetByIdAsync(id);
+            if (property == null)
+            {
+                return NotFound();
+            }
+
+            return View(property);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(Guid id, Property property)
+        {
+            if (id != property.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _propertyService.TUpdateAsync(property);
+                return RedirectToAction("Index");
+            }
+
+            return View(property);
+        }
+
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            Property property = await _propertyService.TGetByIdAsync(id);
+            if (property == null)
+            {
+                return NotFound();
+            }
+
+            return View(property);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            Property property = await _propertyService.TGetByIdAsync(id);
+            if (property == null)
+            {
+                return NotFound();
+            }
+
+            _propertyService.TDelete(property);
+            return RedirectToAction("Index");
+        }
+    }
+}
