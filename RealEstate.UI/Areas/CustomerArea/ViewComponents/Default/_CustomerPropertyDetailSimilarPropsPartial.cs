55cc775d182559ad3b97b8e5f4177435111b9606
TarikAydin29 <you@example.com>
2023-08-17 09:57:24 +0300

    final1

--- RealEstate.UI/Areas/CustomerArea/ViewComponents/Default/_CustomerPropertyDetailSimilarPropsPartial.cs
+++ RealEstate.UI/Areas/CustomerArea/ViewComponents/Default/_CustomerPropertyDetailSimilarPropsPartial.cs
@@ -3,22 +3,16 @@ using RealEstate.BLL.Abstract;
 
 namespace RealEstate.UI.Areas.CustomerArea.ViewComponents.Default
 {
-<<<<<<<< HEAD:RealEstate.UI/Areas/CustomerArea/ViewComponents/Default/_CustomerPropertyDetailSimilarPropsPartial.cs
+
     public class _CustomerPropertyDetailSimilarPropsPartial : ViewComponent
     {
         private readonly IPropertyService propertyService;
 
         public _CustomerPropertyDetailSimilarPropsPartial(IPropertyService propertyService)
-========
-    public class _PropertyDetailSimilarPropsCustomerPartial : ViewComponent
-    {
-        private readonly IPropertyService propertyService;
-
-        public _PropertyDetailSimilarPropsCustomerPartial(IPropertyService propertyService)
->>>>>>>> origin/trk28:RealEstate.UI/Areas/CustomerArea/ViewComponents/Default/_PropertyDetailSimilarPropsCustomerPartial.cs
         {
             this.propertyService = propertyService;
         }
+     
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var properties = await propertyService.TGetAllAsync();
